eb4e8751337fbc16d30c88a0083a933b32035642
Patrick Hladun <patrickhladun@gmail.com>
2024-04-20 14:40:42 +0100

    refactor: rename templates for single and plural talent items

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Talent
 
-def talents(request):
+def talent(request):
     """View function for talent list."""
     talents = Talent.objects.all()
-    return render(request, 'user/talents.html', {'talents': talents})
+    return render(request, 'user/talent.html', {'talents': talents})
 
 
-def talent(request, id):
+def talent_profile(request, id):
     """View function for talent."""
     talent_obj = get_object_or_404(Talent, id=id)
-    return render(request, 'user/talent.html', {'talent': talent_obj})
+    return render(request, 'user/talent-profile.html', {'talent': talent_obj})
