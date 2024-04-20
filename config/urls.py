25a53a725a5d58d67ff266f8f1eb31aee4ff65b2
Patrick Hladun <patrickhladun@gmail.com>
2024-04-20 14:28:26 +0100

    fix: add MEDIA_URL folder to the paths to display images

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -5,4 +7,4 @@ urlpatterns = [
     path('', include('apps.base.urls')),
     path('', include('apps.user.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
