69aecbd8af8cd0f0bab8b5dbd3096d85c6203a45
Patrick Hladun <patrickhladun@gmail.com>
2024-04-30 22:00:44 +0100

    feat: add profile_view, talent_view and account_view

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 from .models import Talent
+from .forms import TalentProfileForm
+
+def talent_view(request, id):
+    """View function for talent single page."""
+    talent = get_object_or_404(Talent, id=id)
+    return render(request, "user/talent.html", {"talent": talent})
+
 
 def talents_view(request):
     """View function for talents list."""
@@ -7,7 +15,41 @@ def talents_view(request):
     return render(request, 'user/talents.html', {'talents': talents})
 
 
-def talent_profile(request, id):
-    """View function for talent."""
-    talent_obj = get_object_or_404(Talent, id=id)
-    return render(request, 'user/talent-profile.html', {'talent': talent_obj})
+def profile_view(request):
+    if not request.user.is_authenticated:
+        return redirect("login")  # Ensure the user is authenticated
+
+    if request.method == "POST":
+        if request.user.user_type == "talent":
+            talent = get_object_or_404(Talent, user=request.user)
+            form = TalentProfileForm(request.POST, request.FILES, instance=talent)
+            template_name = "user/admin/talent_profile.html"
+        else:
+            return redirect("error")
+
+        if form.is_valid():
+            form.save()
+            return redirect("profile")  # Redirect to a success page
+    else:
+        if request.user.user_type == "talent":
+            talent = get_object_or_404(Talent, user=request.user)
+            form = TalentProfileForm(instance=talent)
+            template_name = "user/admin/talent_profile.html"
+        else:
+            return redirect("error")  # Redirect or handle the error
+
+    return render(request, template_name, {"form": form})
+
+
+def account_view(request):
+    if request.method == 'POST':
+        account_form = UpdateAccountForm(request.POST, instance=request.user)
+
+        if account_form.is_valid():
+            account_form.save()
+            messages.success(request, 'Your details were updated successfully')
+            return redirect(to='account')
+    else:
+        account_form = UpdateAccountForm(instance=request.user)
+
+    return render(request, 'user/admin/user_account.html', {'account_form': account_form})
