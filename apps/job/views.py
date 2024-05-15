2e78a0d15561d29cb8e32e06c7d122f471cb6e75
Patrick Hladun <patrickhladun@gmail.com>
2024-05-15 13:29:54 +0100

    feat: add a basic add-job page and view

--- apps/job/views.py
+++ apps/job/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 
 from .models import Listing
+from .forms import JobListingForm
 
 
 def job(request, id):
@@ -22,9 +24,23 @@ def jobs(request):
     return render(request, "job/jobs.html", {"jobs": jobs})
 
 
-
 def my_jobs(request):
     """View function for jobs."""
     jobs = Listing.objects.all()
     return render(request, "job/admin/my_jobs.html", {"jobs": jobs})
+
+@login_required
+def add_job(request):
+    if request.method == 'POST':
+        form = JobListingForm(request.POST)
+        if form.is_valid():
+            new_listing = form.save(commit=False)
+            new_listing.user = request.user  # Set the user as the current logged-in user
+            new_listing.save()
+            return redirect('my-jobs')  # Redirect to a confirmation page or listings overview
+    else:
+        form = JobListingForm()
+
+    return render(request, 'job/admin/add_job.html', {"form": form})
+
 
