f300a28c0a9e65028d5249c974eb5bb97ba82504
Patrick Hladun <patrickhladun@gmail.com>
2024-05-15 13:29:54 +0100

    feat: add a basic add-job page and view

--- apps/job/urls.py
+++ apps/job/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("job/<uuid:id>", views.job, name="job"),
     path("jobs/", views.jobs, name="jobs"),
     path("my-jobs/", views.my_jobs, name="my-jobs"),
+    path("add-job/", views.add_job, name="add-job"),
 ]
