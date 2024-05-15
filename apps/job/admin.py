c21ddfaccb62b3184bcc1828fcae4810f0eaf714
Patrick Hladun <patrickhladun@gmail.com>
2024-05-15 15:16:40 +0100

    refactor: change listing to job except db table, and file names

--- apps/job/admin.py
+++ apps/job/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
-from .models import Listing
+from .models import Job
 # Register your models here.
 
 
-class ListingAdmin(admin.ModelAdmin):
+class JobAdmin(admin.ModelAdmin):
     list_display = ('user', 'title', 'city', 'is_published', 'created_at')
     
 
-
-admin.site.register(Listing, ListingAdmin)
+admin.site.register(Job, JobAdmin)
--- apps/job/views.py
+++ apps/job/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
-from .models import Listing
-from .forms import JobListingForm
+from .models import Job
+from .forms import JobForm
 
 
 def job(request, id):
     """View function for job."""
-    job_obj = get_object_or_404(Listing, id=id)
+    job_obj = get_object_or_404(Job, id=id)
 
     if hasattr(job_obj.user, "employer"):
         employer = job_obj.user.employer
@@ -18,32 +18,36 @@ def job(request, id):
     return render(request, "job/job.html", {"job": job_obj})
 
 
-def jobs(request):
+def job_list(request):
     """View function for jobs."""
-    jobs = Listing.objects.all()
-    return render(request, "job/jobs.html", {"jobs": jobs})
+    jobs = Job.objects.all()
+    return render(request, "job/job_list.html", {"jobs": jobs})
 
 
 @login_required
-def my_jobs(request):
+def user_job_list(request):
     """View function for user-specific job listings."""
     # Filter the listings to only those created by the logged-in user
-    jobs = Listing.objects.filter(user=request.user)
-    return render(request, "job/admin/my_jobs.html", {"jobs": jobs})
+    jobs = Job.objects.filter(user=request.user)
+    return render(request, "job/user/job_list.html", {"jobs": jobs})
 
 
 @login_required
-def add_job(request):
+def user_job_add(request):
     if request.method == 'POST':
-        form = JobListingForm(request.POST)
+        form = JobForm(request.POST)
         if form.is_valid():
             new_listing = form.save(commit=False)
             new_listing.user = request.user  # Set the user as the current logged-in user
             new_listing.save()
-            return redirect('my-jobs')  # Redirect to a confirmation page or listings overview
+            return redirect('user-job-list')  # Redirect to a confirmation page or listings overview
     else:
-        form = JobListingForm()
+        form = JobForm()
 
-    return render(request, 'job/admin/add_job.html', {"form": form})
+    return render(request, 'job/user/job_add.html', {"form": form})
 
+
+@login_required
+def user_job_update(request):
+    return render(request, 'job/user/job_update.html')
 
