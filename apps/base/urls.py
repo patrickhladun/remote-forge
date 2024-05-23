902498599581895bbe8588c9d6d2acb2f7e65c8d
Patrick Hladun <patrickhladun@gmail.com>
2024-05-23 11:04:43 +0100

    feat: create terms and policy pages

--- apps/base/urls.py
+++ apps/base/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('privacy-policy/', views.privacy, name='privacy'),
+    path('terms-conditions/', views.terms, name='terms'),
 ]
--- apps/base/views.py
+++ apps/base/views.py
@@ -7,18 +7,21 @@ def home(request):
     talents = Talent.objects.all()
     return render(request, 'base/home.html', {"talents": talents})
 
-
 def about(request):
     talents = Talent.objects.all()
     return render(request, "base/about.html", {"talents": talents})
 
+def privacy(request):
+    return render(request, "base/privacy.html")
+
+def terms(request):
+    return render(request, "base/terms.html")
 
 def error404(request, *args, **argv):
     response = render('404.html', {},
                                   context_instance=RequestContext(request))
     response.status_code = 404
     return response
-
 
 def error500(request, *args, **argv):
     response = render('500.html', {},
