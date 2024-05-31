f4168c391a15257eb5c5b00b6aa5476e3c21036b
Patrick Hladun <patrickhladun@gmail.com>
2024-05-31 18:11:03 +0100

    feat: add job search form on home and job-list page

--- apps/base/forms.py
+++ apps/base/forms.py
@@ -6,3 +6,7 @@ class ContactForm(forms.Form):
     subject = forms.CharField(max_length=200)
     message = forms.CharField(widget=forms.Textarea)
 
+
+class JobSearchForm(forms.Form):
+    keyword = forms.CharField(max_length=100, required=False)
+    location = forms.CharField(max_length=100, required=False)
