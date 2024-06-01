adffca1e97390e5cd282c7b4e99b4aa6346a2d63
Patrick Hladun <patrickhladun@gmail.com>
2024-06-01 15:16:38 +0100

    feat: add error pages 500, 404, 403

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,15 @@
 from django.conf import settings
+from django.conf.urls import handler404
+from django.shortcuts import render
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from apps.user.views import talent_signup_view, employer_signup_view
+
+def custom_404(request, exception):
+    return render(request, '404.html', status=404)
+
+handler404 = custom_404
 
 urlpatterns = [
     path('', include('apps.base.urls')),
@@ -13,4 +20,7 @@ urlpatterns = [
     path("accounts/signup/talent/", talent_signup_view, name="talent_signup"),
     path("accounts/signup/employer/", employer_signup_view, name="employer_signup",
     ),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
