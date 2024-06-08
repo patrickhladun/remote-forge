3dc2877e582c754811cef6673f7aa12a9d4ce52e
Patrick Hladun <patrickhladun@gmail.com>
2024-06-08 19:44:06 +0100

    test: add contact form tests

--- tests/base/test_base_forms.py
+++ tests/base/test_base_forms.py
@@ -1,6 +1,6 @@
 import pytest
 
-from apps.base.forms import JobSearchForm
+from apps.base.forms import ContactForm, JobSearchForm
 
 
 @pytest.mark.django_db
@@ -29,3 +29,56 @@ class TestJobSearchForm:
         assert (
             "location" not in form.cleaned_data or form.cleaned_data["location"] == ""
         )
+
+
+@pytest.mark.django_db
+class TestContactForm:
+
+    def test_form_initial_state(self):
+        form = ContactForm()
+        assert not form.is_bound
+
+    def test_form_with_valid_data(self):
+        form_data = {
+            "name": "John Doe",
+            "email": "johndoe@example.com",
+            "subject": "Hello",
+            "message": "Hello, World!",
+        }
+        form = ContactForm(data=form_data)
+
+        assert form.is_bound
+        assert form.is_valid()
+        assert form.cleaned_data["name"] == "John Doe"
+        assert form.cleaned_data["email"] == "johndoe@example.com"
+        assert form.cleaned_data["subject"] == "Hello"
+        assert form.cleaned_data["message"] == "Hello, World!"
+
+    def test_form_with_empty_data(self):
+        form_data = {}
+        form = ContactForm(data=form_data)
+
+        assert form.is_bound
+        assert not form.is_valid()
+        assert "name" in form.errors
+        assert "email" in form.errors
+        assert "subject" in form.errors
+        assert "message" in form.errors
+        assert form.errors["name"] == ["This field is required."]
+        assert form.errors["email"] == ["This field is required."]
+        assert form.errors["subject"] == ["This field is required."]
+        assert form.errors["message"] == ["This field is required."]
+
+    def test_form_with_invalid_email(self):
+        form_data = {
+            "name": "John Doe",
+            "email": "john.doe@example",
+            "subject": "Hello",
+            "message": "Hello, World!",
+        }
+        form = ContactForm(data=form_data)
+
+        assert form.is_bound
+        assert not form.is_valid()
+        assert "email" in form.errors
+        assert form.errors["email"] == ["Enter a valid email address."]
