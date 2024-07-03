c937f7e68225474f78f05a6b718df1000e2de5cf
Patrick Hladun <patrickhladun@gmail.com>
2024-07-03 21:15:36 +0100

    refactor: rename test_data, move it to conftest and add single_job data

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,90 @@
+import uuid
+
+import pytest
 from pytest_factoryboy import register
 
-from .factories import EmployerFactory, JobFactory, TalentFactory
+from .factories import (
+    EmployerFactory,
+    JobFactory,
+    TalentFactory,
+    UserEmployerFactory,
+    UserTalentFactory,
+)
 
 register(TalentFactory)
 register(EmployerFactory)
 register(JobFactory)
+
+
+@pytest.fixture
+def test_data_jobs():
+    employer_user_1 = UserEmployerFactory()
+    employer_1 = EmployerFactory(user=employer_user_1, city="Dublin")
+
+    employer_user_2 = UserEmployerFactory()
+    employer_2 = EmployerFactory(user=employer_user_2, city="Galway")
+
+    employer_user_3 = UserEmployerFactory()
+    employer_3 = EmployerFactory(user=employer_user_3, city="Krakow")
+
+    job1 = JobFactory(
+        id=uuid.uuid4(),
+        user=employer_user_1,
+        title="DevOps Engineer",
+        city="Dublin",
+        country="Ireland",
+        salary="60000",
+        schedule="full-time",
+        is_published=True,
+    )
+    job2 = JobFactory(
+        id=uuid.uuid4(),
+        user=employer_user_2,
+        title="Frontend Developer",
+        city="Galway",
+        country="Ireland",
+        salary="60000",
+        schedule="full-time",
+        is_published=True,
+    )
+    job3 = JobFactory(
+        id=uuid.uuid4(),
+        user=employer_user_3,
+        title="Backend Developer",
+        city="Warsaw",
+        country="Poland",
+        salary="60000",
+        schedule="full-time",
+        is_published=True,
+    )
+    job4 = JobFactory(
+        id=uuid.uuid4(),
+        user=employer_user_3,
+        title="Backend Developer",
+        city="Wroclaw",
+        country="Poland",
+        salary="60000",
+        schedule="full-time",
+        is_published=False,
+    )
+
+    return job1, job2, job3, job4
+
+
+@pytest.fixture
+def test_data_single_job():
+    employer_user = UserEmployerFactory()
+    employer = EmployerFactory(user=employer_user, city="Dublin")
+
+    job = JobFactory(
+        id=uuid.uuid4(),
+        user=employer_user,
+        title="DevOps Engineer",
+        city="Dublin",
+        country="Ireland",
+        salary="60000",
+        schedule="full-time",
+        is_published=True,
+    )
+
+    return job
--- tests/job/test_view_job_list.py
+++ tests/job/test_view_job_list.py
@@ -2,79 +2,30 @@ import pytest
 from django.test import Client
 from django.urls import reverse
 
-from apps.job.models import Job
-from apps.user.models import User
-from tests.factories import EmployerFactory, JobFactory, UserEmployerFactory
-
-
-@pytest.fixture
-def test_data():
-    employer_user_1 = UserEmployerFactory()
-    employer_1 = EmployerFactory(user=employer_user_1, city="Dublin")
-
-    employer_user_2 = UserEmployerFactory()
-    employer_2 = EmployerFactory(user=employer_user_2, city="Galway")
-
-    employer_user_3 = UserEmployerFactory()
-    employer_3 = EmployerFactory(user=employer_user_3, city="Krakow")
-
-    job1 = JobFactory(
-        user=employer_user_1,
-        title="DevOps Engineer",
-        city="Dublin",
-        country="Ireland",
-        is_published=True,
-    )
-    job2 = JobFactory(
-        user=employer_user_2,
-        title="Frontend Developer",
-        city="Galway",
-        country="Ireland",
-        is_published=True,
-    )
-    job3 = JobFactory(
-        user=employer_user_3,
-        title="Backend Developer",
-        city="Warsaw",
-        country="Poland",
-        is_published=True,
-    )
-    job4 = JobFactory(
-        user=employer_user_3,
-        title="Backend Developer",
-        city="Wroclaw",
-        country="Poland",
-        is_published=False,
-    )
-
-    return job1, job2, job3, job4
-
 
 @pytest.mark.django_db
-def test_job_list_view_no_filters(test_data):
+def test_job_list_view_no_filters(test_data_jobs):
     """Test job list view without filters. Unpublished jobs should not be displayed."""
     client = Client()
-
     response = client.get(reverse("job-list"))
     assert response.status_code == 200
     assert len(response.context["data"]) == 3
 
 
 @pytest.mark.django_db
-def test_job_list_view_location_filter_not_existing(test_data):
+def test_job_list_view_location_filter_not_existing(test_data_jobs):
     """Test job list view with location filter that does not exist."""
     client = Client()
-
     response = client.get(reverse("job-list") + "?location=NotExisting")
     assert response.status_code == 200
     assert len(response.context["data"]) == 0
 
 
 @pytest.mark.django_db
-def test_job_list_view_location_filter_warsaw(test_data):
+def test_job_list_view_location_filter_warsaw(test_data_jobs):
     """Test job list view with location filter Warsaw."""
     client = Client()
-
+    job1, job2, job3, job4 = test_data_jobs
     response = client.get(reverse("job-list") + "?location=Warsaw")
     assert response.status_code == 200
     assert len(response.context["data"]) == 1
@@ -86,37 +37,33 @@ def test_job_list_view_location_filter_warsaw(test_data):
 
 
 @pytest.mark.django_db
-def test_job_list_view_location_filter_dublin(test_data):
+def test_job_list_view_location_filter_dublin(test_data_jobs):
     client = Client()
-
     response = client.get(reverse("job-list") + "?location=Dublin")
     assert response.status_code == 200
     assert len(response.context["data"]) == 1
 
 
 @pytest.mark.django_db
-def test_job_list_view_location_filter_galway(test_data):
+def test_job_list_view_location_filter_galway(test_data_jobs):
     client = Client()
-
     response = client.get(reverse("job-list") + "?location=Galway")
     assert response.status_code == 200
     assert len(response.context["data"]) == 1
 
 
 @pytest.mark.django_db
-def test_job_list_view_location_filter_ireland(test_data):
+def test_job_list_view_location_filter_ireland(test_data_jobs):
     client = Client()
-
     response = client.get(reverse("job-list") + "?location=Ireland")
     assert response.status_code == 200
     assert len(response.context["data"]) == 2
 
 
 @pytest.mark.django_db
-def test_job_list_view_location_filter_cork(test_data):
+def test_job_list_view_location_filter_cork(test_data_jobs):
     """Test job list view with location filter that does not exist."""
     client = Client()
-
     response = client.get(reverse("job-list") + "?location=Cork")
     assert response.status_code == 200
     assert len(response.context["data"]) == 0
