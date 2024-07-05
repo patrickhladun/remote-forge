b80a5fa703e0a49a3fb890122bc4dcac8a1ebec8
Patrick Hladun <patrickhladun@gmail.com>
2024-07-05 15:14:15 +0100

    test: add tests for account_view

--- tests/user/test_model_talent.py
+++ tests/user/test_model_talent.py
@@ -8,14 +8,12 @@ pytestmark = pytest.mark.django_db
 
 class TestTalentModel:
     def test_create_talent(self, talent_factory):
-        """Test creating a new talent object."""
         talent_user = UserTalentFactory()
         talent = TalentFactory(user=talent_user)
 
         assert talent.id is not None
 
     def test_talent_str_return(self, talent_factory):
-        """Test the string representation of a talent object."""
         talent_user = UserTalentFactory()
         talent = TalentFactory(user=talent_user)
 
