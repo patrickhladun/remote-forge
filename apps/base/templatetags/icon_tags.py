c0c8bd36696d874d92a869acd390af7a74c5edde
Patrick Hladun <patrickhladun@gmail.com>
2024-07-07 20:04:42 +0100

    refactor: change single to double quotes

--- apps/base/templatetags/icon_tags.py
+++ apps/base/templatetags/icon_tags.py
@@ -1,22 +1,24 @@
+import os
+
 from django import template
 from django.utils.safestring import mark_safe
-import os
 
 register = template.Library()
 
+
 @register.simple_tag
-def icon(name, size='md'):
+def icon(name, size="md"):
     sizes = {
-        'sm': 'icon--sm',
-        'md': 'icon--md',
-        'lg': 'icon--lg',
+        "sm": "icon--sm",
+        "md": "icon--md",
+        "lg": "icon--lg",
     }
-    
-    size = sizes.get(size, 'icon--md')
+
+    size = sizes.get(size, "icon--md")
 
     try:
-        with open(os.path.join('static/assets/icons', f'{name}.svg')) as f:
+        with open(os.path.join("static/assets/icons", f"{name}.svg")) as f:
             icon = f.read()
-            return mark_safe(f'<div class="icon {size} text-center">{icon}</div>')
+            return mark_safe(f'<span class="icon {size} text-center">{icon}</span>')
     except FileNotFoundError:
-        return mark_safe('<!-- Icon not found -->')
+        return mark_safe("<!-- Icon not found -->")
