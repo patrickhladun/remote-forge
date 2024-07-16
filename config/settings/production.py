80d18ba5e20af6726d21377e6ca0716696d079ce
Patrick Hladun <patrickhladun@gmail.com>
2024-07-16 12:37:42 +0100

    fix: switch to AWS RDS PostgreSQL Database

--- config/settings/production.py
+++ config/settings/production.py
@@ -8,7 +8,14 @@ heroku_settings(locals(), staticfiles=False)
 DEBUG = False
 ALLOWED_HOSTS = ["remote-forge-a1aedba3d120.herokuapp.com"]
 DATABASES = {
-    "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+         'NAME': os.getenv("DB_NAME"),
+         'USER': os.getenv("DB_USER"),
+         'PASSWORD': os.getenv("DB_PASSWORD"),
+         'HOST': os.getenv("DB_HOST"),
+         'PORT': os.getenv("DB_PORT"),
+     }
 }
 SECRET_KEY = os.getenv("SECRET_KEY", "")
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
